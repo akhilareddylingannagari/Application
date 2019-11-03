05875c653caa9e85db37d40d428b48706f6223ed
charan teja <charan.teja@cashapona.com>
2019-11-03 04:25:26 +0000

    line plot changes

--- src/components/LinePlot.js
+++ src/components/LinePlot.js
@@ -24,129 +24,62 @@ require('highcharts/modules/map')(Highcharts);
 class TrialChart extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      stockOptions: {
-        chart: {
-          zoomType: 'x'
-          //   events: {
-          //     load: function() {
-          //       // set up the updating of the chart each 3 minutes
-          //       var series = this.series[0];
-          //       setInterval(function() {
-          //         var x = new Date().getTime(), // current time
-          //           y = Math.round(Math.random() * 100);
-          //         series.addPoint([x, y], true, true);
-          //       }, 10000);
-          //     }
-          //   }
-        },
 
-        time: {
-          useUTC: false
-        },
+      seriesData: [],
 
-        rangeSelector: {
-          allButtonsEnabled: true,
-          buttons: [
-            {
-              count: 10,
-              type: 'minute',
-              text: 'Today'
-            },
-            {
-              type: 'week',
-              count: 1,
-              text: 'Week'
-            },
-            {
-              type: 'month',
-              count: 1,
-              text: 'Month'
-            },
-            {
-              type: 'year',
-              count: 12,
-              text: 'Year'
-            }
-          ],
-          inputEnabled: true,
-          selected: 0
-        },
 
-        title: {
-          text: this.heading
-        },
-
-        exporting: {
-          enabled: false
-        },
-        yAxis: {
-          opposite: false
-        },
-        navigator: {
-          enabled: false
-        },
-
-        series: [
-          {
-            name: 'newData',
-            data:
-              //  getData(700000)
-
-              (function() {
-                // generate an array of random data
-                var newData = [];
-                fetch(
-                  'https://c7pduqwyg6.execute-api.eu-west-1.amazonaws.com/prod/'
-                )
-                  .then(response => response.json())
-                  .then(res => {
-                    var data = [];
-                    data = res.Items;
-                    console.log('===data', data);
-                    for (var i = 0; i < data.length; i++) {
-                      newData.push([
-                        parseInt(data[i].DateTime),
-                        parseInt(data[i].Grid_Power)
-                      ]);
-                    }
-                    console.log('================', newData);
-                  });
-                return newData;
-              })()
-          }
-        ]
-      }
     };
   }
 
-  componentDidMount() {
-    // fetch('https://c7pduqwyg6.execute-api.eu-west-1.amazonaws.com/prod/')
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     let newData = [];
-    //     console.log('Response data ', data);
-    // for (let i = 0; i < data.length; i++) {
-    //   //X-axis data push
-    //   cat.push(data[i][0]);
-    //   //Y-axis data push
-    //   newData.push({
-    //     y: data[i][1]
-    //   });
-    // }
-    // options.series[0].data = newData;
-    // this.setState({ data: newData });
-    //   });
-    // for (var i = 0; i >= data.Count; i++) {
-    //   newData.push(data[i].DateTime, data[i].Grid_Power);
-    // }
-  }
+  // componentDidMount() {
+  //   let newData = [];
+  //   let temp = [];
+  //   fetch(
+  //     'https://c7pduqwyg6.execute-api.eu-west-1.amazonaws.com/prod/'
+  //   )
+  //     .then(response => response.json())
+  //     .then(res => {
+  //       // console.log(res.Items , "::res")
+  //       let data = res.Items;
+  //       // console.log(data, "asdfds");
+
+  //       this.setState({
+  //         seriesData: data
+  //       })
+
+  //     });
+  // }
+
+
+
+  // componentDidMount() {
+  // fetch('https://c7pduqwyg6.execute-api.eu-west-1.amazonaws.com/prod/')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     let newData = [];
+  //     console.log('Response data ', data);
+  // for (let i = 0; i < data.length; i++) {
+  //   //X-axis data push
+  //   cat.push(data[i][0]);
+  //   //Y-axis data push
+  //   newData.push({
+  //     y: data[i][1]
+  //   });
+  // }
+  // options.series[0].data = newData;
+  // this.setState({ data: newData });
+  //   });
+  // for (var i = 0; i >= data.Count; i++) {
+  //   newData.push(data[i].DateTime, data[i].Grid_Power);
+  // }
+  // }
 
   render() {
-    const { stockOptions } = this.state;
+    // const { stockOptions } = this.state;
+    let stockOptions = getData(this.state.seriesData);
     return (
-      <div>
+      <div style={{width:"100%"}}>
         <StockChart options={stockOptions} highcharts={Highcharts} />
       </div>
     );
@@ -154,3 +87,116 @@ class TrialChart extends React.Component {
 }
 
 export default TrialChart;
+
+
+const getData = (data) => {
+  // console.log(data, "::state");
+
+  let tempData = [];
+  if (data.length > 0) {
+
+    data.map((item, i) => {
+      if (i < 10) {
+        let a = item.DateTime;
+        let b = item.Grid_Power;
+        tempData.push([
+          parseInt(a),
+          parseInt(b)
+        ]);
+      }
+    });
+    console.log(tempData, "::data for chart");
+  }
+
+  let stockOptions = {
+    chart: {
+      zoomType: 'x',
+         events: {
+               load: function () {
+              
+                  // set up the updating of the chart each second
+                  var series = this.series[0];
+                  setInterval(function () {
+                      var x = (new Date()).getTime(), // current time
+                          y = Math.round(Math.random() * 100);
+                      series.addPoint([x, y], true, true);
+                  }, 10000);
+              } 
+          } 
+    },
+  
+    time: {
+      useUTC: false
+    },
+  
+    rangeSelector: {
+      buttons: [{
+          type: 'hour',
+          count: 1,
+          text: '1h'
+        },
+        {
+          type: 'day',
+          count: 1,
+          text: '1d'
+        }, {
+          type: 'week',
+          count: 1,
+          text: 'week'
+        }, {
+          type: 'month',
+          count: 1,
+          text: '1m'
+        }, {
+          type: 'year',
+          count: 1,
+          text: '1y'
+        },
+      ],
+      inputEnabled: false,
+      selected: 0
+    },
+  
+    title: {
+      text: 'Solar Data'
+    },
+  
+    exporting: {
+      enabled: false
+    },
+    xAxis: {
+      type: 'datetime',
+      events: {
+        // afterSetExtremes: afterSetExtremes
+      },
+      dateTimeLabelFormats: {
+            day: '%A , %d %b',
+            week: '%b'
+
+        }
+    },
+    labels: {
+      rotation: 315,
+    },
+  
+    series: [{
+      name: 'Random data',
+      data: (function() {
+        // generate an array of random data
+        var data = [],
+          time = (new Date()).getTime(),
+          i;
+  
+        for (i = -3222222; i <= 0; i += 1) {
+          data.push([
+            time + i * 10000,
+            Math.round(Math.random() * 100)
+          ]);
+        }
+        return data;
+      }())
+    }]
+  }
+
+  return stockOptions;
+}
